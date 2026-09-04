210da6df4d57ad5392ba51f45d6d956459f5fcf0
Blazej Sewera <code@sewera.dev>
2026-09-04 21:20:34 +0200

    test: add more tests for verbosity flag

--- tests/test/mod.rs
+++ tests/test/mod.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use std::{env, fs, path, process, str};
 
 const TESTDATA_DIRNAME: &'static str = "testdata";
--- tests/verbose_param.rs
+++ tests/verbose_param.rs
@@ -8,7 +8,7 @@ const TEST_FILE_CONTENT: &'static str = "file1 test content";
 #[test]
 fn quiet_output() {
     // given
-    let test_name = "rename_one_file";
+    let test_name = "quiet_output";
     let files = vec![test::File {
         name: TEST_FILE_NAME,
         content: TEST_FILE_CONTENT,
@@ -25,4 +25,54 @@ fn quiet_output() {
 
     // then
     assert_eq!(output.trim(), "");
+}
+
+#[test]
+fn verbose_output_param_before_dir() {
+    // given
+    let test_name = "verbose_output_param_before_dir";
+    let files = vec![test::File {
+        name: TEST_FILE_NAME,
+        content: TEST_FILE_CONTENT,
+    }];
+    let renames = vec![test::Rename {
+        from: TEST_FILE_NAME,
+        to: TEST_FILE_NAME_RENAMED,
+    }];
+    test::init(test_name, files);
+    let args = vec!["-v", "."];
+
+    // when
+    let output = test::bulkmv_stdout(args, renames);
+
+    // then
+    assert_eq!(
+        output.trim(),
+        format!("{TEST_FILE_NAME} -> {TEST_FILE_NAME_RENAMED}\n")
+    );
+}
+
+#[test]
+fn verbose_output_param_after_dir() {
+    // given
+    let test_name = "verbose_output_param_after_dir";
+    let files = vec![test::File {
+        name: TEST_FILE_NAME,
+        content: TEST_FILE_CONTENT,
+    }];
+    let renames = vec![test::Rename {
+        from: TEST_FILE_NAME,
+        to: TEST_FILE_NAME_RENAMED,
+    }];
+    test::init(test_name, files);
+    let args = vec![".", "-v"];
+
+    // when
+    let output = test::bulkmv_stdout(args, renames);
+
+    // then
+    assert_eq!(
+        output.trim(),
+        format!("{TEST_FILE_NAME} -> {TEST_FILE_NAME_RENAMED}\n")
+    );
 }
